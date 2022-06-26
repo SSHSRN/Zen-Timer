47aa70622576d3c65d6378a119b94038d9abe730
SRIHARI <sshsrn@gmail.com>
2022-06-26 18:19:49 +0530

    added music effects

--- index.js
+++ index.js
@@ -86,8 +86,8 @@ notification = new Howl({
 	volume: 0.3,
 });
 
-fart = new Howl({
-	src: ["assets/sounds/fart.mp3"],
+wasted = new Howl({
+	src: ["assets/sounds/wasted.mp3"],
 	volume: 2,
 });
 
@@ -138,7 +138,7 @@ function init() {
 	displayAutoStartBreak();
 }
 
-//=========================Initialise Default/Local Storage values=================================================
+// Initialise Default/Local Storage values
 function displayTickSoundValue() {
 	if (localStorage.tickSoundInputValue === "true") {
 		tickSoundInput.checked = localStorage.tickSoundInputValue;
@@ -199,7 +199,7 @@ function displayTodoList() {
 		if (localStorage.todoContents.indexOf("li") === -1) {
 			showNoTaskTodayText();
 		} else {
-			//List is not empty
+			// List is not empty
 			listOfTasks.innerHTML = localStorage.todoContents;
 			removeNoTaskTodayText();
 		}
@@ -292,15 +292,15 @@ function countDown() {
 function resetTimer() {
 	clearInterval(updateSeconds);
 	timerRunning = false;
-	//If user entered some input
+	// If user entered some input
 	if (allPossibleModes[currentTab].localStorage) {
-		//Then use the input the user enters
+		// Then use the input the user enters
 		timeLeft = minutesToSeconds(allPossibleModes[currentTab].localStorage);
 	} else {
-		//Else use default input
+		// Else use default input
 		timeLeft = minutesToSeconds(allPossibleModes[currentTab].defaultTime);
 	}
-	//Display input
+	// Display input
 	timeLeftDisplay.innerHTML = secondsToMinutes(timeLeft);
 	document.title = "Pomodoro Timer Online";
 	progressDisplay();
@@ -358,7 +358,7 @@ function makeTimerButtonActive(buttonClicked) {
 		}
 	});
 }
-//Content Display
+// Content Display
 function contentDisplay() {
 	if (allPossibleModes[currentTab].localStorage) {
 		timeLeft = minutesToSeconds(allPossibleModes[currentTab].localStorage);
@@ -367,7 +367,7 @@ function contentDisplay() {
 	}
 	timeLeftDisplay.innerHTML = secondsToMinutes(timeLeft);
 }
-//When input is updated
+// When input is updated
 pomodoroInput.addEventListener("change", function () {
 	localStorage.currentPomodoroValue = pomodoroInput.value;
 	allPossibleModes["pomodoro"].localStorage = localStorage.currentPomodoroValue;
@@ -391,7 +391,7 @@ function titleTimeDisplay() {
 	titleDisplayText = allPossibleModes[currentTab].titleDisplayText;
 }
 
-//=================Notificiation, Ticking Sounds and Background Music=======================
+// Notificiation, Ticking Sounds and Background Music
 notificationTextInput.addEventListener("change", function () {
 	localStorage.notificationTextInputValue = notificationTextInput.value;
 });
@@ -432,7 +432,7 @@ function stopBackGroundMusic() {
 		allBackgroundMusic[allSounds].stop();
 	}
 }
-//===========Calculate percentage complete for progress bar================================
+// Calculate percentage complete for progress bar
 var degreeOfCircle;
 
 function progressDisplay() {
@@ -453,7 +453,7 @@ function progressDisplay() {
 	}
 }
 
-//=========================Minutes and Seconds converter==========================================
+// Minutes and Seconds converter
 function secondsToMinutes(s) {
 	var minutes = Math.floor(s / 60);
 	var seconds = s % 60;
@@ -470,7 +470,7 @@ function minutesToSeconds(m) {
 	var seconds = m * 60;
 	return seconds;
 }
-//====================================Dark and Light Modes======================================
+// Dark and Light Modes
 darkModeToggle.addEventListener("change", function () {
 	if (darkModeToggle.checked) {
 		darkMode();
@@ -526,7 +526,7 @@ function lightMode() {
 	$(".site-description").css("background", "#9ADBB3");
 	$(".form-control").css("background-color", "#efefef");
 }
-// ================================Get Time and Date=================================================
+// Get Time and Date
 function getDate() {
 	monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 	var today = new Date();
@@ -554,7 +554,7 @@ function getTime() {
 	var time = hours + ":" + minutes + amOrPm;
 	return time;
 }
-// ================================Adding date and time to log=================================================
+// Adding date and time to log
 function addDataToLog() {
 	var sessionsCol = document.createElement("th");
 	sessionsCol.setAttribute("scope", "row");
@@ -602,13 +602,13 @@ function addDataToLog() {
 	storeLogItems();
 	removeNoDataLoggedText();
 }
-// ================================Clear log===================================================
+// Clear log
 clearButton.addEventListener("click", function () {
 	locationUpdateLog.innerHTML = "";
 	storeLogItems();
 	showNoDataLoggedText();
 });
-// ===============================Delete log=================================================
+// Delete log
 function deleteLog(item) {
 	item.parentNode.parentNode.style.transition = "all 0.2s ease-in";
 	item.parentNode.parentNode.classList.add("slide-away");
@@ -620,7 +620,7 @@ function deleteLog(item) {
 		}
 	});
 }
-// ===========================Local Storage for Logging==============================================
+// Local Storage for Logging
 function storeLogItems() {
 	localStorage.logContents = locationUpdateLog.innerHTML;
 }
@@ -629,7 +629,7 @@ function storeLogDescription(item) {
 	item.outerHTML = '<td><input class="form-control" type="text" value="' + item.value + '" onchange="storeLogDescription(this)"></td>';
 	storeLogItems();
 }
-// =====================================No logging data text===============================================
+// No logging data text
 function showNoDataLoggedText() {
 	document.getElementById("NoDataLoggedText").style.display = "block";
 }
@@ -641,7 +641,7 @@ function removeNoDataLoggedText() {
 function logIsEmpty() {
 	return localStorage.logContents.indexOf("tr") === -1;
 }
-//==========================Todo list============================================================
+// Todo list
 var taskInput = document.getElementById("taskInput");
 taskInput.addEventListener("keypress", function () {
 	if (event.key === "Enter") {
@@ -685,7 +685,7 @@ function checkedWhenclicked(item) {
 }
 
 function deleteTasks(item) {
-	fart.play();
+	wasted.play();
 	item.parentElement.style.transition = "all 0.2s ease-in";
 	item.parentElement.classList.add("slide-away");
 	item.parentElement.addEventListener("transitionend", function () {
@@ -711,11 +711,11 @@ function taskMouseOut(item) {
 	item.style.fontSize = "1rem";
 	item.style.transition = "100ms";
 }
-// ================================Local storage for todo list==========================================
+// Local storage for todo list
 function storeTasks() {
 	localStorage.todoContents = listOfTasks.innerHTML;
 }
-// ====================================No task Today Text====================================================
+// No task Today Text
 function showNoTaskTodayText() {
 	document.getElementById("NoTaskTodayText").style.display = "block";
 }
@@ -727,7 +727,7 @@ function removeNoTaskTodayText() {
 function listIsEmpty() {
 	return localStorage.todoContents.indexOf("li") === -1;
 }
-// ======================================Start Next Rounds===================================================
+// Start Next Rounds
 longBreakIntervalInput.addEventListener("input", function () {
 	localStorage.longBreakInterval = Number(longBreakIntervalInput.value);
 	localStorage.sliderValue = Number(longBreakIntervalInput.value);
@@ -786,7 +786,7 @@ function startNextRound() {
 		}
 	}
 }
-// ==================================Auto Start Next Round=====================================
+// Auto Start Next Round
 function autoStartTimer() {
 	if (timerRunning === false) {
 		timerRunning = true;
@@ -809,7 +809,7 @@ function buttonsDefaultState() {
 	stopButton.classList.remove("buttonClicked");
 	resetButton.classList.remove("buttonClicked");
 }
-// ============================Alerts==============================================================
+// Alerts
 var alert = document.querySelector(".alert");
 var alertMessage = document.querySelector("#alertMessage");
 
@@ -835,7 +835,7 @@ function showAlertMessage(session) {
 function dismissAlert() {
 	alert.style.display = "none";
 }
-// ===================Input Validation==================================================
+// Input Validation
 $('input[type="number"]').attr(
 	"onkeypress",
 	"return (event.charCode == 8 || event.charCode == 0) ? null : event.charCode >= 46 && event.charCode <= 57"
